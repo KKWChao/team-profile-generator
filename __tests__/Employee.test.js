650e680d02e835453d550ec4e55251040bdb1d65
Kelvin Chao <kelvinchao2011@gmail.com>
2022-04-03 21:18:46 -0700

    some code

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,7 @@
+const Employee = require('../lib/Employee')
+
+test('Should create an employee object', () => {
+  const employee = new Employee('bobo')
+
+  expect(employee.name).toBe(expect.any(String))
+})
