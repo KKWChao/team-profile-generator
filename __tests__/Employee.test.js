7e596b559b7c9823b507c43ff60257b2001acd96
Kelvin Chao <kelvinchao2011@gmail.com>
2022-04-05 11:09:40 -0700

    fixed some things

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,5 +3,5 @@ const Employee = require('../lib/Employee')
 test('Should create an employee object', () => {
   const employee = new Employee('bobo')
 
-  expect(employee.name).toBe(expect.any(String))
+  expect(employee.name).toBe(expect.any('bobo'))
 })
