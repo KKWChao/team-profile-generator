d756d5de98665098a3017b5662eb8d1cc0123fa0
Kelvin Chao <kelvinchao2011@gmail.com>
2022-04-05 14:54:17 -0700

    added employee methods with test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,37 @@
-const Employee = require('../lib/Employee')
+const Employee = require("../lib/Employee");
 
-// first test for creation
-test('Should create an employee object', () => {
-  const employee = new Employee('Bobo', 1, 'xyz@gmail.com')
+describe("Employee Tests", () => {
+  const employee = new Employee("Bobo", 1, "xyz@gmail.com");
 
-  expect(employee.name).toEqual(expect.any(String))
-  expect(employee.id).toEqual(expect.any(Number))
-  expect(employee.email).toEqual(expect.any(String))
-})
+  it("Should create an employee object", () => {
+    
+    expect(employee.name).toEqual('Bobo');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual("xyz@gmail.com");
+    expect(employee.role).toEqual("Employee");
+  });
+
+  it('Should Return name', () => {
+    expect(employee.getName())
+      
+    .toEqual(employee.name)
+  })
+
+  it('Should return ID', () => {
+    expect(employee.getID())
+    
+    .toEqual(employee.id)
+  })
+
+  it('Should return email', () => {
+    expect(employee.getEmail())
+    
+    .toEqual(employee.email)
+  })
+
+  it('Should return current role', ()=> {
+    expect(employee.getRole())
+
+    .toEqual(employee.role)
+  })
+});
