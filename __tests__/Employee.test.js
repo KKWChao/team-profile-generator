50c2747fe5223030309894c9d326f99ccb93e00e
Kelvin Chao <kelvinchao2011@gmail.com>
2022-04-05 11:28:31 -0700

    .

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,10 @@
 const Employee = require('../lib/Employee')
 
+// first test for creation
 test('Should create an employee object', () => {
-  const employee = new Employee('bobo')
+  const employee = new Employee('Bobo', 1, 'xyz@gmail.com')
 
-  expect(employee.name).toBe(expect.any('bobo'))
+  expect(employee.name).toEqual(expect.any(String))
+  expect(employee.id).toEqual(expect.any(Number))
+  expect(employee.email).toEqual(expect.any(String))
 })
