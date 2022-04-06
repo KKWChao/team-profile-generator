93b13d59ff0c0f21cb467781fe62c6037fb6d0c4
Kelvin Chao <kelvinchao2011@gmail.com>
2022-04-06 13:44:25 -0700

    fixing script for template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,69 @@
+// ? why doesnt require import work?
 
-// const data = require('../../lib/Builder')
 
 var main_Container = document.getElementById("main-container");
+var demoData = [
+  {name:'Alpha', id:1, email:'xyz@gmail.com', role:'Manager'},
+  {name:'Bravo', id:2, email:'xyz@gmail.com', role:'Engineer'},
+  {name:'Charlie', id:3, email:'xyz@gmail.com', role:'Intern'},
+  {name:'Delta', id:4, email:'xyz@gmail.com', role:'Engineer'}]
 
 function cardCreator(data) {
-  let count = 10;
+  // update counter with input later
+  let count = 5;
 
   for (let i = 0; i < count; i++) {
     var card = document.createElement("div");
     card.classList.add("card");
     card.style.width = "15rem";
-
-    var cardImage = document.createElement("img");
-    cardImage.classList.add("card-img-top");
-    cardImage.src = "#";
+    card.setAttribute('card', i)
 
     var cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
-
+    
+    // update name with input later
     var cardTitle = document.createElement("h5");
     cardTitle.classList.add("card-title");
     cardTitle.classList.add("text-center")
-    cardTitle.innerHTML = "Test";
+    cardTitle.setAttribute('card-name', i)
+    cardTitle.innerHTML = `${data[i].name}`;
 
     var cardText = document.createElement("p");
     cardText.classList.add("card-text");
     cardText.classList.add("text-center")
+    cardText.setAttribute('card-text', i)
     cardText.innerHTML =
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeat dolore in, accusamus ut consequuntur nobis ex illum quasi neque laboriosam. Minus harum illo cupiditate laborum nostrum beatae illum aliquam.";
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+
+    var cardListUl = document.createElement('ul')
+    cardListUl.classList.add('list-group')
 
 
+    // update ID with input later
+    var cardListID = document.createElement('li')
+    cardListID.classList.add('list-group-item')
+    cardListID.setAttribute('card-id', i)
+    cardListID.innerHTML = `ID: ${data[i].id}` 
 
+    // update Email with input later
+    var cardListEmail = document.createElement('li')
+    cardListEmail.classList.add('list-group-item')
+    cardListEmail.setAttribute('card-email', i)
+    cardListEmail.innerHTML = `Email: ${data[i].email}`
 
+    // var cardListLi = document.createElement('li')
+    // cardListLi.classList.add('list-group-item')
 
     main_Container.append(card);
 
-    card.append(cardImage);
     card.append(cardBody);
 
     cardBody.append(cardTitle);
     cardBody.append(cardText);
+    cardBody.append(cardListUl)
+
+    cardListUl.append(cardListID)
+    cardListUl.append(cardListEmail)
   }
 }
-
-cardCreator();
+cardCreator(demoData)
